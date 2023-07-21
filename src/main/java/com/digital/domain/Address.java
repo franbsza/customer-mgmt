ad2b3c796602b15aa89b8fad2fc885686f3529bf
franbsza <franciele.bsza@gmail.com>
2023-07-20 22:03:04 -0300

    refactor: adapt domains to spring upgraded

--- src/main/java/com/digital/domain/Address.java
+++ src/main/java/com/digital/domain/Address.java
@@ -1,12 +1,13 @@
 package com.digital.domain;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import jakarta.persistence.*;
+
 
 @Entity
 @Table(name ="address")
